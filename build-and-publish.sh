2aeadaacd964254c5cb960dcc700cf0beef49c69
Anoosha Raj <anoosharaj@Anooshas-MacBook-Air.local>
2019-07-28 22:12:35 -0700

    Updated the package version

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,11 +1,16 @@
 npm run build:package
 npm run build:docs
 
+git status
+git add --all
+git commit --all --message=$1
+
 [ -z "$2" ] && npm version patch || npm version $2
 #npm publish --access public
 
+git status
 git add --all
-git commit --all --message=$1
+git commit --all --message="Updated the package version"
 git push
 
 
